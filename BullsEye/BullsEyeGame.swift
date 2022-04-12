e6492fee00e488ad0029e922ed58efcf8c651a83
Martin Pilch <mpilch@mindmed.co>
2022-04-12 13:26:12 +0200

    Refactoring indentations, UI and code style

--- BullsEye/BullsEyeGame.swift
+++ BullsEye/BullsEyeGame.swift
@@ -1,34 +1,34 @@
 import Foundation
 
 class BullsEyeGame {
-  var round = 0
-  let startValue = 50
-  var targetValue = 50
-  var scoreRound = 0
-  var scoreTotal = 0
-  
-  init() {
-    startNewGame()
-  }
-  
-  func startNewGame() {
-    round = 0
-    scoreTotal = 0
-    startNewRound()
-  }
-  
-  func startNewRound() {
-    round = round + 1
-    scoreRound = 0
-    targetValue = 1 + (Int(arc4random()) % 100)
-  }
+    var round = 0
+    let startValue = 50
+    var targetValue = 50
+    var scoreRound = 0
+    var scoreTotal = 0
 
-  @discardableResult
-  func check(guess: Int) -> Int {
-    let difference = guess - targetValue
-    scoreRound = 100 - difference
-    scoreTotal = scoreTotal + scoreRound
-    
-    return difference
-  }
+    init() {
+        startNewGame()
+    }
+
+    func startNewGame() {
+        round = 0
+        scoreTotal = 0
+        startNewRound()
+    }
+
+    func startNewRound() {
+        round = round + 1
+        scoreRound = 0
+        targetValue = 1 + (Int(arc4random()) % 100)
+    }
+
+    @discardableResult
+    func check(guess: Int) -> Int {
+        let difference = guess - targetValue
+        scoreRound = 100 - difference
+        scoreTotal = scoreTotal + scoreRound
+
+        return difference
+    }
 }
--- BullsEye/PlayersViewController.swift
+++ BullsEye/PlayersViewController.swift
@@ -1,73 +1,70 @@
 import UIKit
 
 class PlayersViewController: UIViewController {
-  @IBOutlet weak var player1Name: UILabel!
-  @IBOutlet weak var player1Address: UILabel!
-  @IBOutlet weak var player2Name: UILabel!
-  @IBOutlet weak var player2Address: UILabel!
-  
-  
-  var players: [Player]?
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    players = []
-    
-    self.populateInitialData()
-    self.updateUI()
-  }
-  
-  private func populateInitialData() {
-    let address = Address(fullAddress: "123 Tutorial Street")
-    players?.append(Player(name: "Peter", address: address))
-    players?.append(Player(name: "Martin", address: address))
-  }
-  
-  private func updateUI() {
-    player1Name.text = players?[0].name
-    player1Address.text = players?[0].address.fullAddress
-    player2Name.text = players?[1].name
-    player2Address.text = players?[1].address.fullAddress
-  }
-  
-  private func showAlert(player: Player){
-    let alert = UIAlertController(title: "Change address", message: "Enter new address for player: \(player.name)", preferredStyle: .alert)
-    
-    alert.addTextField { (textField) in
-      textField.accessibilityIdentifier = "addressInput"
-      textField.placeholder = "Enter new address"
+    @IBOutlet weak var player1Name: UILabel!
+    @IBOutlet weak var player1Address: UILabel!
+    @IBOutlet weak var player2Name: UILabel!
+    @IBOutlet weak var player2Address: UILabel!
+
+    var players: [Player]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        players = []
+
+        self.populateInitialData()
+        self.updateUI()
+    }
+
+    private func populateInitialData() {
+        let address = Address(fullAddress: "123 Tutorial Street")
+        players?.append(Player(name: "Peter", address: address))
+        players?.append(Player(name: "Martin", address: address))
+    }
+
+    private func updateUI() {
+        player1Name.text = players?[0].name
+        player1Address.text = players?[0].address.fullAddress
+        player2Name.text = players?[1].name
+        player2Address.text = players?[1].address.fullAddress
+    }
+
+    private func showAlert(player: Player){
+        let alert = UIAlertController(title: "Change address", message: "Enter new address for player: \(player.name)", preferredStyle: .alert)
+
+        alert.addTextField { (textField) in
+            textField.accessibilityIdentifier = "addressInput"
+            textField.placeholder = "Enter new address"
+        }
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = alert?.textFields?[0]
+            player.address.fullAddress = textField?.text ?? ""
+            self.updateUI()
+        }))
+
+        self.present(alert, animated: true, completion: nil)
     }
     
-    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
-      let textField = alert?.textFields![0]
-      player.address.fullAddress = textField!.text ?? ""
-      self.updateUI()
-    }))
-    
-    self.present(alert, animated: true, completion: nil)
-  }
-  
-  func loadPlayerPhotos() {
-    if let player1 = players?[0] {
-      player1.didGetPhotos = { (photos) in
-        self.updatePlayerPhotos(photos)
-      }
-      
-      player1.getPhotos()
+    func loadPlayerPhotos() {
+        if let player1 = players?[0] {
+            player1.didGetPhotos = { (photos) in
+                self.updatePlayerPhotos(photos)
+            }
+
+            player1.getPhotos()
+        }
     }
-  }
-  
-  private func updatePlayerPhotos(_ photos: Array<Any>) {
-    // update photos of player
-  }
-  
-  @IBAction func changePlayer1Address(_ sender: Any) {
-    showAlert(player: players![0])
-  }
-  
-  
-  @IBAction func changePlayer2Address(_ sender: Any) {
-    showAlert(player: players![1])
-  }
-  
+
+    private func updatePlayerPhotos(_ photos: Array<Any>) {
+        // update photos of player
+    }
+
+    @IBAction func changePlayer1Address(_ sender: Any) {
+        showAlert(player: players![0])
+    }
+
+    @IBAction func changePlayer2Address(_ sender: Any) {
+        showAlert(player: players![1])
+    }
 }
